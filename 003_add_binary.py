fc272d4bc8c414367a3f33c5f664ae36238b6c99
Sanket Patel <sanpatel323@gmail.com>
2026-06-22 15:03:50 +0530

    Added 003_add_binary solution

--- 003_add_binary.py
+++ 003_add_binary.py
@@ -13,4 +13,21 @@ Approaches:
   3. Bit manipulation without full string conversion  ->  O(max(n,m)) time, O(max(n,m)) space
 """
 
+
 # TODO: implement your solution here
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        s = []
+        carry = 0
+        i = len(a) - 1
+        j = len(b) - 1
+        while i >= 0 or j >= 0 or carry:
+            if i >= 0:
+                carry += int(a[i])
+                i -= 1
+            if j >= 0:
+                carry += int(b[j])
+                j -= 1
+            s.append(str(carry % 2))
+            carry = carry // 2
+        return "".join(reversed(s))
